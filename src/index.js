3b46b419d9a5291050e8685fccf1389630d31276
Narges1374 <narges.timachi@gmail.com>
2023-04-20 18:01:58 +0430

    add relevant icon

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
+function formatdate(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let currentDay = days[date.getDay()];
+  let currentHours = date.getHours();
+  if (currentHours < 10) {
+    currentHours = `0${currentHours}`;
+  }
+  let currentMinutes = date.getMinutes();
+  if (currentMinutes < 10) {
+    currentMinutes = `0${currentMinutes}`;
+  }
+  return `${currentDay} ${currentHours}:${currentMinutes}`;
+}
 
 function showTemp(response) {
   document.querySelector("h1").innerHTML = response.data.name;
-  document.querySelector("#day-hour").innerHTML=formatdate(response.data.dt * 1000)
-  document.querySelector("#description").innerHTML = response.data.weather[0].description;
+  document.querySelector("#day-hour").innerHTML = formatdate(
+    response.data.dt * 1000
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].description;
   document.querySelector("#degree").innerHTML = Math.round(
     response.data.main.temp
   );
@@ -10,6 +35,12 @@ function showTemp(response) {
   document.querySelector("#Wind").innerHTML = Math.round(
     response.data.wind.speed
   );
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchCity(city) {
@@ -37,31 +68,5 @@ function getCurrentLocation(event) {
 }
 let currentLocationButton = document.querySelector("#current-location");
 currentLocationButton.addEventListener("click", getCurrentLocation);
-
-
-function formatdate(timestamp) {
-  let date = new Date(timestamp);
-
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let currentDay = days[date.getDay()];
-  let currentHours = date.getHours();
-  if (currentHours < 10) {
-    currentHours = `0${currentHours}`;
-  }
-  let currentMinutes = date.getMinutes();
-  if (currentMinutes < 10) {
-    currentMinutes = `0${currentMinutes}`;
-  }
-  return `${currentDay} ${currentHours}:${currentMinutes}`;
-}
-
 
 searchCity("Tehran");
